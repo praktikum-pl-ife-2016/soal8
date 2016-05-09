b944f64aae6e5f3e21c3d6cad163f1e66df3c17c
rizkiramadhan2 <rizki2500@gmail.com>
2016-05-09 22:35:49 +0700

    upload kelas turunan

---
+++ src/Mobil.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author R-PC
+ */
+public class Mobil extends KendaraanDarat {
+    public void nama(){
+        System.out.println("Nama       : Mobil");
+    }
+    public void noProduksi(){
+        System.out.println("No Prod    : 4574");
+    }
+    public void manufaktur(){
+        System.out.println("Manufaktur : Honda");
+    }
+    public void warna(){
+        System.out.println("Warna      : Merah");
+    }
+    public void bahanBakar(){
+        System.out.println("Kendaraan ini menggunakan bahan bakar Bensin");
+    }
+}
