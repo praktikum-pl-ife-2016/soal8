1e6483953c878bb8f5297da54497f67e0c7d7886
luthfipratama <luthfip20.lp@gmail.com>
2016-05-10 00:02:48 +0700

    upload interface class

---
+++ src/Kendaraan.java
@@ -0,0 +1,8 @@
+public interface Kendaraan {
+public void nama();
+public void nomorProduksi();
+public void manufaktur();
+public void warna();
+public void caraops();
+public void bahanbakar();
+}
