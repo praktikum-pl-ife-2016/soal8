f883597c17798716cba61d1701b61228834afde6
afifdwhy <dwimurtadhaafif@yahoo.com>
2016-05-10 00:18:26 +0700

    Add files via upload

---
+++ src/kendaraanGetek.java
@@ -0,0 +1,31 @@
+
+
+public class kendaraanGetek extends kendaraanLaut {
+
+    @Override
+    public void nama() {
+        System.out.println("Nama: Getek");
+    }
+
+    @Override
+    public void nomor() {
+        System.out.println("Nomor Produksi: 007");
+
+    }
+
+    @Override
+    public void manufaktur() {
+        System.out.println("Manufaktur: Surabaya");
+    }
+
+    @Override
+    public void warna() {
+        System.out.println("Warna: Bebas");
+    }
+
+    @Override
+    public void tenaga() {
+        System.out.println(" dan menggunakan tenaga tangan");
+    }
+
+}
