55e9423461fee13ade76ef120638e4ea0f7100bb
luthfipratama <luthfip20.lp@gmail.com>
2016-05-10 00:03:47 +0700

    upload superclass

---
+++ src/KapalLaut.java
@@ -0,0 +1,8 @@
+public class KapalLaut extends KendaraanLaut {
+        public KapalLaut(String nm, String noP, String Manufak, String War) {
+        super(nm, noP, Manufak, War);
+    }
+    public void bahanbakar(){
+        System.out.println("Solar atau sejenis Bensin");
+    }
+}
---
+++ src/Mobil.java
@@ -0,0 +1,9 @@
+public class Mobil extends KendaraanDarat {
+    
+    public Mobil(String nm, String noP, String Manufak, String War) {
+        super(nm, noP, Manufak, War);
+    }
+    public void bahanbakar(){
+        System.out.println("Bensin,Solar atau Diesel");
+    }
+}
